e7a84e98461f57343052642594a836e538da5124
Padmanabhan <padmanabhansridhar2005@gmail.com>
2025-06-14 13:00:03 +0530

    feat: project init with auth

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+}
